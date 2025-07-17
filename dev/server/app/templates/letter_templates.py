86510a95dafd69512a210c27408ed3387a130783
inesha21 <e19090@eng.pdn.ac.lk>
2025-07-17 07:53:12 +0530

    add a leave template

--- dev/server/app/templates/letter_templates.py
+++ dev/server/app/templates/letter_templates.py
@@ -2,7 +2,7 @@ TEMPLATES = {
     "offer": """Dear {candidateName},
 
 We are pleased to offer you the position of {jobRole} in our {department} department at LEARN, starting on {startDate}.
-Your salary will be {salary} per month. {notes}.
+Your salary will be {salary} per month.
 
 We look forward to working with you.
 
@@ -20,5 +20,14 @@ We wish you the best in your job search.
 Best Regards,
 HR Department,
 LEARN
+""",
+    "leave":"""Dear {managerName}, 
+    
+I hope this message finds you well. I am writing to request a {leaveType} Leave for {numberOfDays} days from {startDate} to {endDate}.
+The reason for my leave is {reason}. Thank you for understanding.
+
+Best Regards,
+{employeeName}
 """
+
 }

a79ae6c9dabb0e9eb9521f9cfd7b0eb3ff03dd4c
inesha21 <e19090@eng.pdn.ac.lk>
2025-07-17 06:51:16 +0530

    hide navbar in login and register states

--- dev/client/src/App.jsx
+++ dev/client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
@@ -11,11 +11,19 @@ import Chat from "./pages/Chat";
 import ViewProfile from "./pages/ViewProfile";
 import EditProfile from "./pages/EditProfile";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { useAuth } from "./hooks/useAuth";
 
-function App() {
+function AppWrapper() {
+  const location = useLocation();
+  const { isAuthenticated } = useAuth();
+
+  // Hide Navbar on login/register routes
+  const hideNavbarOn = ["/login", "/"];
+  const hideNavbar = hideNavbarOn.includes(location.pathname) || !isAuthenticated;
+
   return (
-    <BrowserRouter>
-      <Navbar />
+    <>
+      {!hideNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<RegisterForm />} />
         <Route path="/login" element={<LoginForm />} />
@@ -58,6 +66,14 @@ function App() {
           element={<ProtectedRoute element={EditProfile} />}
         />
       </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppWrapper />
     </BrowserRouter>
   );
 }
